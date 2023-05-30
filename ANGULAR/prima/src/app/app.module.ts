a745adc2baa2c26fb9eb21cfe5d2f298289efb18
Marcotmn <129113184+Marcotmn@users.noreply.github.com>
2023-05-30 16:13:13 +0200

    PRIMO ANGULAR LEZIONE1

--- ANGULAR/prima/src/app/app.module.ts
+++ ANGULAR/prima/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { SecondoComponent } from './secondo/secondo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    SecondoComponent,
   ],
   imports: [
     BrowserModule
